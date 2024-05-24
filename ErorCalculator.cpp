e1a48dae80f76097b6328fc9a75c9375b614891e
Nonbot5690 <slavayninricardo@gmail.com>
2024-05-24 20:59:46 +0300

    Add functional into ErrorCalculator

--- ErorCalculator.cpp
+++ ErorCalculator.cpp
@@ -1,1 +1,28 @@
 #include "ErorCalculator.h"
+
+ErrorCalculator::ErrorCalculator(const c_double* firstVector, const c_double* secondVector, int sizeOfVectors)
+{
+	CalculateAverageError(firstVector, secondVector, sizeOfVectors);
+}
+
+void ErrorCalculator::CalculateAverageError(const c_double* firstVector, const c_double* secondVector, int sizeOfVectors)
+{
+	for (int k = 0; k < sizeOfVectors; k++)
+	{
+		m_erRe += std::abs(firstVector[k].real() - secondVector[k].real());
+		m_erIm += std::abs(firstVector[k].imag() - secondVector[k].imag());
+	}
+
+	m_erRe = m_erRe / static_cast<double>(sizeOfVectors);
+	m_erIm = m_erIm / static_cast<double>(sizeOfVectors);
+}
+
+double ErrorCalculator::GetRealError()
+{
+	return m_erRe;
+}
+
+double ErrorCalculator::GetImagError()
+{
+	return m_erIm;
+}
--- ErorCalculator.h
+++ ErorCalculator.h
@@ -1,44 +1,39 @@
 #pragma once
-class ErorCalculator
+
+#include <complex>
+
+#define c_double std::complex<double>
+
+class ErrorCalculator
 {
 public:
 
-	void SetAverageErrorValues()
-	{
-		for (int k = 0; k < m_N; k++)
-		{
-			m_erFreqRe += std::abs(m_fValIn[k].real() - m_fValOut[k].real());
-			m_erFreqIm += std::abs(m_fValIn[k].imag() - m_fValOut[k].imag());
+	ErrorCalculator(const c_double* firstVector, const c_double* secondVector, int sizeOfVectors);
 
-			m_erTimeRe += std::abs(m_tValIn[k].real() - m_tValOut[k].real());
-			m_erTimeIm += std::abs(m_tValIn[k].imag() - m_tValOut[k].imag());
-		}
+	void CalculateAverageError(const c_double* firstVector, const c_double* secondVector, int sizeOfVectors);
 
-		m_erFreqRe = m_erFreqRe / static_cast<double>(m_N);
-		m_erFreqIm = m_erFreqIm / static_cast<double>(m_N);
+	template <typename vector>
+	double GetAveregeErrorByVectors(const vector firstVector, const vector secondVector, int sizeOfVectors);
 
-		m_erTimeRe = m_erTimeRe / static_cast<double>(m_N);
-		m_erTimeIm = m_erTimeIm / static_cast<double>(m_N);
+	double GetRealError();
+	double GetImagError();
 
-	}
-
-	std::cout << "========================FFT value eror========================\n";
-
-	std::cout << "The Average Eror of forward FFT for Real frequency components: " << m_erFreqRe << "\n";
-	std::cout << "The Average Eror of forward FFT for Imag frequency components: " << m_erFreqIm << "\n";
-
-	std::cout << "The Average Eror of inverse FFT for Real time components: " << m_erTimeRe << "\n";
-	std::cout << "The Average Eror of inverse FFT for Imag time components: " << m_erTimeIm << "\n";
-
-	std::cout << "========================FFT value eror========================\n";
 
 private:
-	//the average error between the input and output variables
-	double m_erFreqRe = 0;
-	double m_erFreqIm = 0;
 
-	double m_erTimeRe = 0;
-	double m_erTimeIm = 0;
+	//the average error between the input and output variables
+	double m_erRe = 0; // average error of real values
+	double m_erIm = 0; // average erroe of imaginary values
 
 };
 
+template<typename vector>
+inline double ErrorCalculator::GetAveregeErrorByVectors(const vector firstVector, const vector secondVector, int sizeOfVectors)
+{
+	double result = 0;
+
+	for (int k = 0; k < sizeOfVectors; k++)
+		result += std::abs(firstVector[k] - secondVector[k]);
+	
+	return (result / static_cast<double>(sizeOfVectors));
+}
